23ed59c0d003f8946792bd37e0df04c2cf114462
LeonelCedillo <leonel.cs.979@gmail.com>
2025-12-10 22:31:47 -0800

    encode image to base64 string and store in SQLite instead

--- src/api/thumbnails.ts
+++ src/api/thumbnails.ts
@@ -4,38 +4,7 @@ import { getVideo, updateVideo } from "../db/videos";
 import type { ApiConfig } from "../config";
 import type { BunRequest } from "bun";
 import { BadRequestError, NotFoundError, UserForbiddenError } from "./errors";
-import { getInMemoryURL } from "./assets";
 
-type Thumbnail = {
-  data: ArrayBuffer;
-  mediaType: string;
-};
-
-const videoThumbnails: Map<string, Thumbnail> = new Map();
-
-export async function handlerGetThumbnail(cfg: ApiConfig, req: BunRequest) {
-  const { videoId } = req.params as { videoId?: string };
-  if (!videoId) {
-    throw new BadRequestError("Invalid video ID");
-  }
-
-  const video = getVideo(cfg.db, videoId);
-  if (!video) {
-    throw new NotFoundError("Couldn't find video");
-  }
-
-  const thumbnail = videoThumbnails.get(videoId);
-  if (!thumbnail) {
-    throw new NotFoundError("Thumbnail not found");
-  }
-
-  return new Response(thumbnail.data, {
-    headers: {
-      "Content-Type": thumbnail.mediaType,
-      "Cache-Control": "no-store",
-    },
-  });
-}
 
 export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
   const { videoId } = req.params as { videoId?: string };
@@ -72,18 +41,17 @@ export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
     throw new BadRequestError("Missing Content-Type for thumbnail");
   }
 
-  const fileData = await file.arrayBuffer(); // array of bytes
+  const fileData = await file.arrayBuffer(); // file data as array of bytes
   if (!fileData) {
     throw new Error("Error reading file data");
   }
-  
-  videoThumbnails.set(videoId, {
-    data: fileData,
-    mediaType
-  } satisfies Thumbnail);
 
-  const urlPath = getInMemoryURL(cfg, videoId);
-  video.thumbnailURL = urlPath;
+  // raw bytes to Node's optimized binary type with convinient helpers like .toString()
+  const base64Encoded = Buffer.from(fileData).toString("base64"); 
+  const base64DataURL = `data:${mediaType};base64,${base64Encoded}`;
+
+  video.thumbnailURL = base64DataURL;
+  
   updateVideo(cfg.db, video);
 
   return respondWithJSON(200, video);
